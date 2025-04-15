67e10ac68156a74f6d3c2c3cf99288f65f44c3fa
LakshayMohan <lakshaymohan.21@gmail.com>
2025-04-15 23:37:42 +0530

    adding routes for chat bot , faq and removing history routes and its component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import TaxCalculator from './TaxCalculator';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Chatbot from './components/Chatbot';
-import FAQs from './components/FAQs';
+import History from './components/History';
 import './App.css';
-
-function AppContent() {
-    const location = useLocation();
-    // Show chatbot only on /calculator route
-    const showChatbot = location.pathname === '/calculator';
-
-    return (
-        <div className="app">
-            <Header />
-            <main className="main-content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/calculator" element={<TaxCalculator />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path ="/faqs" element={<FAQs/>} />
-                </Routes>
-            </main>
-            <Chatbot visible={showChatbot} />
-        </div>
-    );
-}
 
 function App() {
     return (
         <Router>
-            <AppContent />
+            <div className="app">
+                <Header />
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/calculator" element={<TaxCalculator />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/history" element={<History />} />
+                    </Routes>
+                </main>
+            </div>
         </Router>
     );
 }
 
-export default App;
+export default App; 
